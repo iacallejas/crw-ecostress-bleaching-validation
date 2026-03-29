d3b9028bc307b7dd4c76d33f720e1daa35d40575
Ileana Galdamez (Callejas) <63425874+iacallejas@users.noreply.github.com>
2026-03-28 20:33:47 -0700

    Add files via upload

---
+++ download_dhw_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the base URL and destination path for data
+BASE_URL="https://www.star.nesdis.noaa.gov/pub/socd/mecb/crw/data/5km/v3.1_op/nc/v1.0/daily/dhw"
+DEST_PATH=""
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DEST_PATH"
+
+# Loop through the years 1985 to 2016
+for YEAR in {1985..2024}; do
+    echo "Downloading SST data for year: $YEAR"
+    wget -r -np -nd -A "*.nc" -P "$DEST_PATH" "$BASE_URL/$YEAR/"
+done
